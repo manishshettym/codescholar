cf7b0330245790386a12ab7b458340d40599b662
manishshettym <manishs@berkeley.edu>
2022-09-02 13:59:01 -0700

    rewrite placeholder test to pass

--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -3,7 +3,5 @@ import unittest
 class PlaceHolderTests(unittest.TestCase):
     def placeholder_test(self):
         # some computation
-        a, b, c = 1, 2, 3
-
-        # assertion for test
-        self.assertTrue(c == a+b)
+        
+        pass
