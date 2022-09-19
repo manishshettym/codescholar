d45a9d52b0dde0e12ac5cdfced62f7cdb1606fd1
manishshettym <manishs@berkeley.edu>
2022-09-19 11:05:44 -0700

    update experiment prog

--- codescholar/experiments/idiom.py
+++ codescholar/experiments/idiom.py
@@ -1,2 +1,6 @@
-with open(file) as filepointer:
-    line = filepointer.read()
+# with open(file) as filepointer:
+#     line = filepointer.read()
+
+with open(file) as fp:
+    line = fp.read()
+    inp = line.split()
