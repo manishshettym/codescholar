14799176cb37d6a06f362db0e7bc3ec2507c9937
manishshettym <manishs@berkeley.edu>
2022-09-27 14:30:31 -0700

    add new code utils for normalized parsing/unparsing

---
+++ codescholar/utils/code_utils.py
@@ -0,0 +1,29 @@
+import ast
+import astunparse
+import black
+
+
+def normalize_code_str(code: str) -> str:
+    """
+    Returns a formatting-normalized version of the code by running through
+    a parser and then a code-generator.
+    Args:
+        code: A string corresponding to the code to normalize
+    Returns:
+        (str): The normalized code.
+    """
+    return normalize_code_ast(ast.parse(code))
+
+
+def normalize_code_ast(code_ast: ast.AST) -> str:
+    """
+    Returns a formatting-normalized version of the provided Python AST by
+    running through a code-generator.
+    Args:
+        code_ast: The Python AST to unparse.
+    Returns:
+        (str): A normalized code string.
+    """
+    mode = black.FileMode()
+    result = black.format_str(astunparse.unparse(code_ast).strip(), mode=mode)
+    return result.strip()
