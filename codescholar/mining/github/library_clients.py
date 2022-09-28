91bd7ee5cea174bd92b45e105065c5a7e4c086ec
manishshettym <manishs@berkeley.edu>
2022-09-27 19:48:32 -0700

    add multiproc for miner

--- codescholar/mining/github/library_clients.py
+++ codescholar/mining/github/library_clients.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import git
 import json
 
@@ -7,6 +6,10 @@ from tqdm import tqdm
 import glob
 import shutil
 from typing import List
+
+from codescholar.utils import multiprocess
+
+MAX_WORKERS = 2
 
 
 def load_repository_paths(path: str):
@@ -30,48 +33,61 @@ def is_library_used(filepath: str, lib: str):
     return False
 
 
+def _repo_lib_clients_mp(args):
+    repo_path, lib = args
+    return repo_lib_clients(repo_path, lib)
+
+
+def repo_lib_clients(repo_path: str, lib: str):
+    data_dir = "../../data/github/"
+    clone_loc = f"{data_dir}/{lib}"
+
+    repo_name = repo_path.split("/")[-1]
+    repo_loc = os.path.join(clone_loc, repo_name)
+    has_lib_usage = False
+
+    # create scratchpad directory for copying
+    temp_dir = f"{data_dir}/{repo_name}-temp"
+    os.makedirs(temp_dir)
+
+    # clone repo
+    git.Git(clone_loc).clone(f"https://github.com/{repo_path}")
+
+    # find lib usages
+    for path in glob.glob(f"{repo_loc}/**", recursive=True):
+
+        if os.path.isfile(path) and path.endswith(".py"):
+
+            if is_library_used(path, lib):
+
+                filename = os.path.basename(path)
+                destination = os.path.join(temp_dir, filename)
+                shutil.move(path, destination)
+
+                has_lib_usage = True
+
+    # remove repo
+    shutil.rmtree(repo_loc)
+
+    # rename or remove scratchpad
+    if has_lib_usage:
+        os.rename(temp_dir, os.path.join(data_dir, lib, repo_name))
+    else:
+        shutil.rmtree(temp_dir)
+
+
 def get_library_clients(paths: List[str], lib: str):
     data_dir = "../../data/github/"
     clone_loc = f"{data_dir}/{lib}"
-    temp_dir = f"{data_dir}/temp"
 
-    # create campaign directory:
     if not os.path.isdir(f'{clone_loc}'):
         os.makedirs(clone_loc)
 
-    for repo_path in tqdm(paths):
-
-        # create scratchpad directory for copying
-        os.makedirs(temp_dir)
-
-        # clone repo
-        git.Git(clone_loc).clone(f"https://github.com/{repo_path}")
-
-        repo_name = repo_path.split("/")[-1]
-        repo_loc = os.path.join(clone_loc, repo_name)
-        has_lib_usage = False
-
-        # find lib usages
-        for path in glob.glob(f"{repo_loc}/**", recursive=True):
-
-            if os.path.isfile(path) and path.endswith(".py"):
-
-                if is_library_used(path, lib):
-
-                    filename = os.path.basename(path)
-                    destination = os.path.join(temp_dir, filename)
-                    shutil.move(path, destination)
-
-                    has_lib_usage = True
-
-        # remove repo
-        shutil.rmtree(repo_loc)
-
-        # rename or remove scratchpad
-        if has_lib_usage:
-            os.rename(temp_dir, os.path.join(data_dir, lib, repo_name))
-        else:
-            shutil.rmtree(temp_dir)
+    multiprocess.run_tasks_in_parallel(
+        _repo_lib_clients_mp,
+        tasks=[(path, lib) for path in paths],
+        use_progress_bar=True,
+        num_workers=MAX_WORKERS)
 
 
 if __name__ == "__main__":
