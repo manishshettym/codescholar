5d425a5aa7a95596c55b9c5184d43b001fb0f0d0
manishshettym <manishs@berkeley.edu>
2023-03-30 20:40:02 -0700

    evaluation script + bumped up max idiom size

--- codescholar/evaluation/evaluate.py
+++ codescholar/evaluation/evaluate.py
@@ -5,26 +5,35 @@ import os.path as osp
 
 import torch
 
+from codescholar.sast.simplified_ast import get_simplified_ast
 from codescholar.sast.visualizer import render_sast
 from codescholar.representation import config
 from codescholar.search.search import main as search_main
 from codescholar.search import search_config
 
+
 def main(args):
-    # load the benchmarks
-    # with open('benchmarks.json') as f:
-    #     benchmarks = json.load(f)
+    with open('benchmarks.json') as f:
+        benchmarks = json.load(f)
 
-    # for lib, apis in benchmarks.items():
-    #     for api in apis:
+    for lib in benchmarks:
+        for api in benchmarks[lib]:
+            print(f"EVALUATING [{lib}] [{api}]")
+            print("=====================================")
 
-    #         # set seed argument for search
-    #         args.seed = '{}.{}'.format(lib, api)
+            args.seed = api
+            args.result_dir = f"./results/{args.seed}/" if args.mode == 'g' else "./results/"
+            args.idiom_g_dir = f"{args.result_dir}/idioms/graphs/"
+            args.idiom_p_dir = f"{args.result_dir}/idioms/progs/"
 
-    #         # call search_main
-    #         search_main(args)
-    
-    search_main(args)
+            if not osp.exists(args.idiom_g_dir):
+                os.makedirs(args.idiom_g_dir)
+
+            if not osp.exists(args.idiom_p_dir):
+                os.makedirs(args.idiom_p_dir)
+
+            search_main(args)
+            print("=====================================\n\n")
 
 
 if __name__ == "__main__":
@@ -33,24 +42,15 @@ if __name__ == "__main__":
     config.init_encoder_configs(parser)
     search_config.init_search_configs(parser)
     args = parser.parse_args()
-    
+
     # data config
     args.prog_dir = f"../data/{args.dataset}/source/"
     args.source_dir = f"../data/{args.dataset}/graphs/"
     args.emb_dir = f"../search/tmp/{args.dataset}/emb/"
-    args.result_dir = f"./results/{args.seed}/" if args.mode == 'g' else "./results/"
-    args.idiom_g_dir = f"{args.result_dir}/idioms/graphs/"
-    args.idiom_p_dir = f"{args.result_dir}/idioms/progs/"
 
     # model config
     args.test = True
     args.model_path = f"../representation/ckpt/model.pt"
 
-    if not osp.exists(args.idiom_g_dir):
-        os.makedirs(args.idiom_g_dir)
-    
-    if not osp.exists(args.idiom_p_dir):
-        os.makedirs(args.idiom_p_dir)
-    
     torch.multiprocessing.set_start_method('spawn')
     main(args)
