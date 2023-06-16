6effda1787f3b63fe1350714bf05a2bb2154be71
manishshettym <manishs@berkeley.edu>
2023-06-16 18:58:27 +0000

    fix error: np.int -> int

--- codescholar/utils/search_utils.py
+++ codescholar/utils/search_utils.py
@@ -61,7 +61,7 @@ def vec_hash(v):
 def wl_hash(g, dim=64):
     """weisfeiler lehman graph hash"""
     g = nx.convert_node_labels_to_integers(g)
-    vecs = np.zeros((len(g), dim), dtype=np.int)
+    vecs = np.zeros((len(g), dim), dtype=int)
 
     for v in g.nodes:
         if g.nodes[v]["anchor"] == 1:
@@ -69,7 +69,7 @@ def wl_hash(g, dim=64):
             break
 
     for i in range(len(g)):
-        newvecs = np.zeros((len(g), dim), dtype=np.int)
+        newvecs = np.zeros((len(g), dim), dtype=int)
         for n in g.nodes:
             newvecs[n] = vec_hash(np.sum(vecs[list(g.neighbors(n)) + [n]], axis=0))
         vecs = newvecs
