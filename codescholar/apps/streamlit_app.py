159fd6fb4bb69cab8031d72311f6e3f203fe7492
manishshettym <manishs@berkeley.edu>
2023-06-22 00:00:14 +0000

    making changes to app

--- codescholar/apps/streamlit_app.py
+++ codescholar/apps/streamlit_app.py
@@ -1,38 +1,50 @@
 from collections import namedtuple
 import altair as alt
 import math
+import json
 import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+<img align="center" src="https://github.com/tart-proj/codescholar/blob/ms/evaluation/codescholar.png"/>
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+Find the best code idioms for your task using CodeScholar! CodeScholar not only 
+finds the best code idioms for your task, but also provides you with the **provenance** -- links to the
+real-world code snippets that use the idiom!
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+Go ahead and try it out! Select an API from the dropdown menu below and see what idioms CodeScholar finds for you!
 """
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+with open("benchmarks.json") as f:
+    benchmarks = json.load(f)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+api_options = set()
+for lib in benchmarks:
+    for api in benchmarks[lib]:
+        api_options.add(api)
 
-    points_per_turn = total_points / num_turns
+option = st.selectbox(
+    'Which API do you want to search for?',
+    api_options)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# with st.echo(code_location='below'):
+#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+#     Point = namedtuple('Point', 'x y')
+#     data = []
+
+#     points_per_turn = total_points / num_turns
+
+#     for curr_point_num in range(total_points):
+#         curr_turn, i = divmod(curr_point_num, points_per_turn)
+#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+#         radius = curr_point_num / total_points
+#         x = radius * math.cos(angle)
+#         y = radius * math.sin(angle)
+#         data.append(Point(x, y))
+
+#     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+#         .mark_circle(color='#0068c9', opacity=0.5)
+#         .encode(x='x:Q', y='y:Q'))
