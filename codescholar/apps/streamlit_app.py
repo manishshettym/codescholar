b09a24feae57c2a856e1923d3a093a952085149f
manishshettym <manishs@berkeley.edu>
2023-06-22 00:05:33 +0000

    fix path for app

--- codescholar/apps/streamlit_app.py
+++ codescholar/apps/streamlit_app.py
@@ -16,7 +16,7 @@ Go ahead and try it out! Select an API from the dropdown menu below and see what
 """
 
 
-with open("benchmarks.json") as f:
+with open("../evaluation/benchmarks.json") as f:
     benchmarks = json.load(f)
 
 api_options = set()
