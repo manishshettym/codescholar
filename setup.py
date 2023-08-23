5b4ffd5d420dc78bf92502e6c929c0c057243170
manishshettym <manishs@berkeley.edu>
2023-08-23 11:01:45 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name="CodeScholar",
     version="0.0.0",
     packages=find_packages(where="."),
-    url="https://github.com/manishshettm/codescholar",
+    url="https://github.com/tart-proj/codescholar",
     license="BSD-2-Clause",
     author="Manish Shetty",
     author_email="manishs@berkeley.edu",
-    description="Your new favorite programming assistant",
+    description="Your favorite idiomatic code example search engine",
 )
